e73acfd55aba98c714f72b238c0865856026605b
Abir Taheer <abir@taheer.me>
2025-06-22 21:41:03 -0400

    exclude the redirect endpoint from swagger

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,11 @@
 import { Controller, Get, Res } from "@nestjs/common";
+import { ApiExcludeEndpoint } from "@nestjs/swagger";
 import { Response } from "express";
 
 @Controller()
 export class AppController {
-  @Get("/")
+  @Get()
+  @ApiExcludeEndpoint(true)
   redirectToSwagger(@Res() res: Response) {
     return res.redirect(302, "/api");
   }
