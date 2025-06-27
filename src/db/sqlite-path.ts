918217674208bf54ff31592eeefdbdd35349263d
Abir Taheer <abir@taheer.me>
2025-06-27 00:38:32 -0400

    remove /dist from db path

--- src/db/sqlite-path.ts
+++ src/db/sqlite-path.ts
@@ -2,4 +2,4 @@ import { srcRoot } from "@/src-root";
 import { resolve } from "path";
 
 // if the /dist gets added to the path, we remove it
-export const sqlitePath = resolve(srcRoot, "../app.db");
+export const sqlitePath = resolve(srcRoot, "../app.db").replace("/dist", "");
